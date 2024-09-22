453c0b3de1b8f13e20cac21e0060e5177e38134d
BuildTools <unconfigured@null.spigotmc.org>
2024-09-23 02:04:11 +0700

    t1

--- bashsprint1.sh
+++ bashsprint1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
-nvm install --lts
+nvm install 18
+nvm use 18
 node -e "console.log('Running Node.js ' + process.version)"
 
 
   echo "Installing yarn..."
   
-  yum install yarn
+  npm install yarn -g
   rm yarn.lock
 
 #cd testsprint1/
