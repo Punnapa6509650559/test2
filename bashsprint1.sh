ed52bb01b07a46ba42d7ce5d3b43832a56d669bf
toonneedmoneyyyy <punnapa.tri@dome.tu.ac.th>
2024-09-23 00:30:37 +0700

    f

--- bashsprint1.sh
+++ bashsprint1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -o- -L https://yarnpkg.com/install.sh | bash
 node --max-old-space-size=8192 index.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
@@ -7,8 +8,8 @@ node -e "console.log('Running Node.js ' + process.version)"
 
 
   echo "Installing yarn..."
-  curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
-  sudo yum install yarn
+ # curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
+#  sudo yum install yarn
   rm yarn.lock
 
 #cd testsprint1/
