2a33f1a30859f04d450be24ac23107e69db83894
toonneedmoneyyyy <punnapa.tri@dome.tu.ac.th>
2024-09-23 00:50:22 +0700

    ip

--- src/http.js
+++ src/http.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-var url="34.239.126.140";
+var url = "44.204.148.47";
 
 export default axios.create({
 
-    baseURL: "http://" + url +":1337/",
+    baseURL: "http://" + url + ":1337/",
     headers: {
         "Content-type": "application/json"
     }
