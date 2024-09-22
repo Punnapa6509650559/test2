cad512ecaa1bab4bcd44aa572b31845259b75923
toonneedmoneyyyy <punnapa.tri@dome.tu.ac.th>
2024-09-23 01:06:07 +0700

    test

--- bashsprint1.sh
+++ bashsprint1.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-curl -o- -L https://yarnpkg.com/install.sh | bash
-node --max-old-space-size=8192 index.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-nvm install --lts
-node -e "console.log('Running Node.js ' + process.version)"
 
-sudo npm install
-  echo "Installing yarn..."
- # curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
- sudo npm install -g  yarn
-  rm yarn.lock
 
-#cd testsprint1/
+
+curl -sL https://dl.yarnpkg.com/install.sh | bash
+
+
+cd /path/to/your/project
+
 
 yarn install
 
-yarn start
+yarn run start
+
